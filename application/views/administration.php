db1be6d2bc842f09a129f7310d4db3e875485b3d
Dr. Naveen Tewari <navtewari@gmail.com>
2019-01-23 19:57:38 +0530

    up
    up

--- application/views/administration.php
+++ application/views/administration.php
@@ -14,20 +14,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12 about-right px-lg-5 px-2 mt-lg-2 mt-2">
-                        <h3 class="tittle"><span class="sub-tittle-w3">Academics in</span>Gurukul Academy</h3>
-                    </div>
-                    <div class="col-lg-6 about-right">
-                        <div class="card-deck mt-5">                        
-                            <div class="card">                            
-                                <div class="card-body p-lg-5">
-                                    <h3 class="tittle"><span class="sub-tittle-w3">SCHOOL TEACHING PLAN</span></h3>
-                                    <p class="card-text"><br/>
-                                        The School has three distinct divisions / sections of classes, viz. JUNIOR SECTION, MIDDLE SECTION .Nursery to V std form the Junior Section, VI std to VIII std form the middle section. Functioning like two small schools within the school, each section is headed by a senior teacher who oversees the day to day teaching work of each section. The Class Teachers under her/him get guidance from the sectional Head. Teacher regarding teaching method & completion of syllabus in a planned manner.</p>
-                                        <p class="card-text">The two sectional heads report to the Principal about the progress or difficulties faced by them as the case may be. The responsibility under the teaching plan is thus decentralized down the line. The ultimate responsible unit in such scheme rests with the Class Teacher whose interaction with the students being the maximum. </p>
-                                </div>  
-                            </div> 
-                        </div>
-                    </div>
+                        <h3 class="tittle"><span class="sub-tittle-w3">Administration in</span>Gurukul Academy</h3>
+                    </div>                    
 
                     <div class="col-lg-12 about-right">
                         <div class="card-deck mt-5">                        
