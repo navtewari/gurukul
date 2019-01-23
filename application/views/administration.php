538c1eb2d0b8717b76ccc60e46a5c6391d048c17
Dr. Naveen Tewari <navtewari@gmail.com>
2019-01-23 19:56:54 +0530

    up
    up

--- application/views/administration.php
+++ application/views/administration.php
@@ -29,27 +29,26 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-6 about-right">
+                    <div class="col-lg-12 about-right">
                         <div class="card-deck mt-5">                        
                             <div class="card">                            
                                 <div class="card-body p-lg-5">
-                                    <h3 class="tittle"><span class="sub-tittle-w3">SALIENT FEATURES & FACILITIES</span></h3>
+                                    <h3 class="tittle"><span class="sub-tittle-w3">GENERAL RULES</span></h3>
                                     <p class="card-text">
                                         <ul class="list-group">
-                                            <li class="list-group-item list-group-item-success">Highly Qualified, versatile, stable teaching faculty.</li>
-                                            <li class="list-group-item  list-group-item-danger">Lush Green, Pollution free zone sprawling over 7 acres area.</li>
-                                            <li class="list-group-item list-group-item-success">Smart Classes</li>
-                                            <li class="list-group-item  list-group-item-danger">Audio - Visual Hall (for dramatics, screening of films etc.)</li>
-                                            <li class="list-group-item list-group-item-success">Skating Rink</li>
-                                            <li class="list-group-item  list-group-item-danger">Well developed fields for hockey, basketball, cricket, football, badminton etc.</li>
-                                            <li class="list-group-item list-group-item-success">Library cum Reading room.</li>
-                                            <li class="list-group-item  list-group-item-danger">Amphi-theatre for inter-house Activities.</li>
-                                            <li class="list-group-item list-group-item-success">Caring behaviour of the teaching staff towards children.</li>
-                                            <li class="list-group-item  list-group-item-danger">Thrust on instilling in the young generation time honoured sanskars and desirable attributes of character.</li>
-                                            <li class="list-group-item list-group-item-success">Special attention on Personality Development and on spoken English.</li>
-                                            <li class="list-group-item  list-group-item-danger">Play way method of teaching for Nursery kids.</li>
-                                            <li class="list-group-item list-group-item-success">Well equipped Computer Lab.</li>
-                                            <li class="list-group-item  list-group-item-danger">Multiroute bus facility available (for all connecting routes)</li>
+                                            <li class="list-group-item list-group-item-success">Children must be in the School Campus at least ten minutes before the time fixed for the school to commence.</li>
+                                            <li class="list-group-item  list-group-item-danger">Attendance in School should be regular. Leave of absence must be applied for which may/ may not be granted</li>
+                                            <li class="list-group-item list-group-item-success">Children are not allowed to wear jewellery or bring valuables to School. The School is not responsible for the loss of jewellery, money or OTHER such valuables brought to school by the children.</li>
+                                            <li class="list-group-item  list-group-item-danger">Children should be habitually clean and neatly dressed. Uniform is to be worn on all days.</li>
+                                            <li class="list-group-item list-group-item-success">Running, playing, shouting and unruly behavior inside School building is not allowed.</li>
+                                            <li class="list-group-item  list-group-item-danger">Parents and guardians are not permitted to enter classrooms during class hours without permission.</li>
+                                            <li class="list-group-item list-group-item-success">The name and class of the pupil should be clearly marked on all his/ her belongings</li>
+                                            <li class="list-group-item  list-group-item-danger">No Pupil may leave the School premises during the School hours and interval without the prior permission of Principal.</li>
+                                            <li class="list-group-item list-group-item-success">The School reserves the right to ask the parents to withdraw their children if their progress in studies is not satisfactory, their attendance is below 75% or their conduct is not good towards OTHER students.</li>
+                                            <li class="list-group-item  list-group-item-danger">Honesty, cleanliness of dress and loyalty to school are demanded and expected of each student.</li>
+                                            <li class="list-group-item list-group-item-success">Immorality, insubordination, contempt of authority or damage to property will be a sufficient reason for immediate expulsion of the student.</li>
+                                            <li class="list-group-item  list-group-item-danger">Students are admitted in the school at their own risk. School will not be responsible for any injury or loss of books and stationery.</li>
+                                            <li class="list-group-item list-group-item-success">Student is not allowed to carry mobile phone in the school.</li>                                           
                                         </ul>
                                     </p>
                                 </div>  
