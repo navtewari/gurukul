18f3cd1343f4af88ff15b9383f9c2e3e5d77fa1b
Dr. Naveen Tewari <navtewari@gmail.com>
2019-01-28 10:32:38 +0530

    up
    up

--- application/views/miscellaneous.php
+++ application/views/miscellaneous.php
@@ -15,15 +15,63 @@
                 <div class="row">
                     <div class="col-lg-12 about-right px-lg-5 px-2 mt-lg-2 mt-2">
                         <h3 class="tittle"><span class="sub-tittle-w3"></span>Miscellaneous</h3>
-                    </div>
+                    </div>                    
+
                     <div class="col-lg-6 about-right">
                         <div class="card-deck mt-5">                        
                             <div class="card">                            
                                 <div class="card-body p-lg-5">
-                                    <h3 class="tittle"><span class="sub-tittle-w3">SCHOOL TEACHING PLAN</span></h3>
+                                    <h3 class="tittle"><span class="sub-tittle-w3">SPORTS & CO-CURRICULAR ACTIVITIES</span></h3>
                                     <p class="card-text"><br/>
-                                        The School has three distinct divisions / sections of classes, viz. JUNIOR SECTION, MIDDLE SECTION .Nursery to V std form the Junior Section, VI std to VIII std form the middle section. Functioning like two small schools within the school, each section is headed by a senior teacher who oversees the day to day teaching work of each section. The Class Teachers under her/him get guidance from the sectional Head. Teacher regarding teaching method & completion of syllabus in a planned manner.</p>
-                                        <p class="card-text">The two sectional heads report to the Principal about the progress or difficulties faced by them as the case may be. The responsibility under the teaching plan is thus decentralized down the line. The ultimate responsible unit in such scheme rests with the Class Teacher whose interaction with the students being the maximum. </p>
+                                        Sports and co-curricular activities are conducted under the supervision of Physical Education Teachers. Keeping in view the age group of children, they are encouraged for playing different games accordingly. Martial Arts training is to be introduced in the 2016-17 session.</p>
+                                    <p class="card-text">Sport / Games facilities are provided at the school under two categories :</p>
+                                    <div id="accordion">
+                                        <div class="card">
+                                            <div class="card-header" id="headingOne">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                    Indoor
+                                                </button>
+                                                </h5>
+                                            </div>
+
+                                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                                <div class="card-body">
+                                                    <ul class="list-group list-group-item-success">
+                                                        <li class="list-group-item">Billiards</li>
+                                                        <li class="list-group-item">Table Tennis</li>
+                                                        <li class="list-group-item">Chess</li>
+                                                        <li class="list-group-item">Boxing</li>
+                                                        <li class="list-group-item">Gymnastics</li>
+                                                        <li class="list-group-item">Carrom</li>
+                                                        <li class="list-group-item">Skipping Rope</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card">
+                                            <div class="card-header" id="headingTwo">
+                                                <h5 class="mb-0">
+                                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                    Outdoor
+                                                </button>
+                                                </h5>
+                                            </div>
+                                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                                <div class="card-body">
+                                                   <ul class="list-group list-group-item-success">
+                                                        <li class="list-group-item">Cricket</li>
+                                                        <li class="list-group-item">Football</li>
+                                                        <li class="list-group-item">Volleyball</li>
+                                                        <li class="list-group-item">Basketball</li>
+                                                        <li class="list-group-item">Badminton</li>
+                                                        <li class="list-group-item">Skating</li>
+                                                        <li class="list-group-item">Kho-Kho</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>                                        
+                                    </div>                                    
                                 </div>  
                             </div> 
                         </div>
@@ -33,25 +81,62 @@
                         <div class="card-deck mt-5">                        
                             <div class="card">                            
                                 <div class="card-body p-lg-5">
-                                    <h3 class="tittle"><span class="sub-tittle-w3">SALIENT FEATURES & FACILITIES</span></h3>
+                                    <h3 class="tittle"><span class="sub-tittle-w3">A REQUEST TO PARENTS / GUARDIANS</span></h3><br/>                                    
+                                    <p class="card-text"><br/>
+                                        Any school / school necessarily needs the cooperation of parents / guardians to properly groom & educate the children. The studies at school have to be supplemented with adequate study time at home. Parents have to ensure that children do not waste time as the syllabus for all classes is voluminous under the present day education system. </p>
                                     <p class="card-text">
-                                        <ul class="list-group">
-                                            <li class="list-group-item list-group-item-success">Highly Qualified, versatile, stable teaching faculty.</li>
-                                            <li class="list-group-item  list-group-item-danger">Lush Green, Pollution free zone sprawling over 7 acres area.</li>
-                                            <li class="list-group-item list-group-item-success">Smart Classes</li>
-                                            <li class="list-group-item  list-group-item-danger">Audio - Visual Hall (for dramatics, screening of films etc.)</li>
-                                            <li class="list-group-item list-group-item-success">Skating Rink</li>
-                                            <li class="list-group-item  list-group-item-danger">Well developed fields for hockey, basketball, cricket, football, badminton etc.</li>
-                                            <li class="list-group-item list-group-item-success">Library cum Reading room.</li>
-                                            <li class="list-group-item  list-group-item-danger">Amphi-theatre for inter-house Activities.</li>
-                                            <li class="list-group-item list-group-item-success">Caring behaviour of the teaching staff towards children.</li>
-                                            <li class="list-group-item  list-group-item-danger">Thrust on instilling in the young generation time honoured sanskars and desirable attributes of character.</li>
-                                            <li class="list-group-item list-group-item-success">Special attention on Personality Development and on spoken English.</li>
-                                            <li class="list-group-item  list-group-item-danger">Play way method of teaching for Nursery kids.</li>
-                                            <li class="list-group-item list-group-item-success">Well equipped Computer Lab.</li>
-                                            <li class="list-group-item  list-group-item-danger">Multiroute bus facility available (for all connecting routes)</li>
-                                        </ul>
+                                        We advise the parents to ensure the following:
+                                    </p>    
+                                    <ul class="list-group" style="font-size: .9em;">
+                                        <li class="list-group-item list-group-item-danger">Look at the student's school diary every day & see to it that the home work assigned for the next day is done.</li>
+                                        <li class="list-group-item list-group-item-danger">Fix certain hours for studying at home & be strict with the child to observe this routine.</li>
+                                        <li class="list-group-item list-group-item-danger">Encourage your child to develop neatness as habit. The child should come to school in clean  and tidy uniform, polished shoes and hair properly combed.</li>
+                                        <li class="list-group-item list-group-item-danger">Parents should avoid to criticize the teacher or the school in the presence of the children. If there is a grievance or a complaint, please talk to the school management directly.</li>
+                                        <li class="list-group-item list-group-item-danger">As most of the children come from Hindi speaking families, children should be encouraged to do a certain amount of regular english conversation at home.</li>
+                                        <li class="list-group-item list-group-item-danger">Parents should be strict with children as far as going to school is concerned. Regular school attendance IS A MUST. Absence from school on trivial ground will not be allowed.</li>
+                                        <li class="list-group-item list-group-item-danger">The school's day to day functioning and growth is financed by the fees paid by you. Please ensure timely payment of fees.</li>
+
+                                    </ul>                               
+                                </div>  
+                            </div> 
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 about-right">
+                        <div class="card-deck mt-5">                        
+                            <div class="card">                            
+                                <div class="card-body p-lg-5">
+                                    <h3 class="tittle"><span class="sub-tittle-w3">House System</span></h3>
+                                    <p class="card-text"><br/>
+                                        The student community has been divided into four Houses viz., [Takshshila House], [Nalanda House], [Ajanta House] and [Tagore House] respectively. Every student is placed in one of these Houses on admission. Each House is guarded by a House Captain (student) and supported by a House Master (teacher). Each House has a different colour for its dress code which the students have to wear on saturdays.</p>
+                                    <p class="card-text">
+                                        Inter-House competitions amongst different Houses are held in Sports, General Knowledge, Dramatics, Debates, quiz competition etc.
                                     </p>
+                                    <ol class="list-group">
+                                        <li class="list-group-item list-group-item-success">Four Houses</li>
+                                        <ul class="list-group">
+                                            <li class="list-group-item">Takshshila House</li>
+                                            <li class="list-group-item">Nalanda House</li>
+                                            <li class="list-group-item">Ajanta House</li>
+                                            <li class="list-group-item">Tagore House</li>                                           
+                                        </ul>
+                                    </ol>
+                                </div>  
+                            </div> 
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 about-right">
+                        <div class="card-deck mt-5">                        
+                            <div class="card">                            
+                                <div class="card-body p-lg-5">
+                                    <h3 class="tittle"><span class="sub-tittle-w3">EDUCATIONAL TOURS & SCHOOL TRANSPORTATION</span></h3><br/>
+                                    <img src="<?php echo base_url(). 'assets/images/bus.jpg'?>" class="img-fluid img-thumbnail" />
+                                    <p class="card-text"><br/>
+                                        School has a multi-route bus transportation system with affordable transportation charges. New buses have been pressed into service with expert drivers on duty for the safety of the children. </p>
+                                    <p class="card-text">
+                                        Educational Tours and picnics are arranged for all the three wings of the institution i.e. Junior and Middle wings. Educational tours for Forest Research Institute Dehradun and nearby industrial units have been arranged which were much appreciated & enjoyed by children.
+                                    </p>                                   
                                 </div>  
                             </div> 
                         </div>
